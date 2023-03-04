e08d042a0c04acf5a2465220b91337d349a69b93
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2023-03-04 14:41:34 -0300

    Update EmpregodaFaculdade.java

--- EmpregodaFaculdade.java
+++ EmpregodaFaculdade.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
 public class EmpregodaFaculdade {
-    private String nome;
-    private double salario;
-    private int horasAula;
 
-    double getGastos (){
-        return this.salario + 40;
+    public static void main(String[] args) {
+
+        String nomeProfessor;
+        double salario;
+        int horasAula;
+        int horasTrabalhadas;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe o nome do Professor: ");
+        nomeProfessor= ler.next();
+
+        System.out.println("Informe a o salário: ");
+        salario = ler.nextDouble();
+
+        System.out.println("Informa quantidade de horas trabalhadas: ");
+        horasAula = ler.nextInt();
+
+        salario = (horasAula*40)+salario;
+
+        System.out.println("O salario final do professor é: " + salario);
+
+
     }
-    public void bonus (double salario){
-        salario = salario+horasAula;
-    }
-
-
-    String getInfo(){
-        return "nome:" + this.nome + "com salario" + this.salario;
-    }
-
 }
-
